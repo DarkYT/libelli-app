8abd217f771aaecf3271678bacd1746cd106db5d
DarkYT <marius.verdier@hotmail.fr>
2021-11-08 21:19:30 +0100

    Security

---
+++ lib/config.dart
@@ -0,0 +1,1 @@
+String GOOGLE_API_KEY = "AIzaSyAsL5Qc5T-16oIiny5v5UJDEH2KffDNYbU";
--- lib/helpers/requests.dart
+++ lib/helpers/requests.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 
+import 'package:app_livres/config.dart';
 import 'package:http/http.dart' as http;
 
 Future<String> fetchBookDetails(isbn) async {
-  final response = await http.get(Uri.parse('https://www.googleapis.com/books/v1/volumes?q=isbn:'+isbn+'&key=AIzaSyAsL5Qc5T-16oIiny5v5UJDEH2KffDNYbU'));
+  final response = await http.get(Uri.parse('https://www.googleapis.com/books/v1/volumes?q=isbn:'+isbn+'&key='+GOOGLE_API_KEY));
 
   if(response.statusCode == 200) {
     return jsonDecode(response.body).toString();
